18180e803af11add327bc5f6e02fdf86fcbfbf72
oscarpw <oscar.castillo@primeraweb.cl>
2019-07-11 05:11:26 -0400

    ToDo App at 80% finish. CSS pending

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+    get 'todos', to: 'todos#index'
+    get 'todos/new'
+    post 'todos',to: 'todos#create'
+    get 'todos/:id', to: 'todos#show', as: 'todo'
+    get 'todos/:id/edit', to: 'todos#edit', as: 'editing'
+    patch 'todos/:id', to: 'todos#update'
+    delete 'todos/:id', to: 'todos#destroy'
+    get 'todos/:id/complete', to: 'todos#complete', as:'completed'
+    get 'todos/list'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
