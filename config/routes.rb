0360463810e985bee6f4b95151d28c322acb4ba3
oscarpw <oscar.castillo@primeraweb.cl>
2019-07-11 21:31:00 -0400

    Finally 2.0  1000% Go! Go!

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
     get 'todos/new'
     get 'todos/list'
     post 'todos',to: 'todos#create'
+    post 'todos/:id/complete', to: 'todos#complete', as:'complete_todo'
     get 'todos/:id', to: 'todos#show', as: 'todo'
     get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
     patch 'todos/:id', to: 'todos#update'
     delete 'todos/:id', to: 'todos#destroy'
-    get 'todos/:id/complete', to: 'todos#complete', as:'complete_todo'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
